be27798ce351cefd7f5f23a3899c1bf4ff14dd0f
Alex <alexanderchapman@outlook.com>
2018-04-15 23:22:23 +0100

    feat: created function to flip card.
    Created the first function to flip the cards. It has a number of things still required. At the moment it only flips the cards with toggle class.

--- js/app.js
+++ js/app.js
@@ -36,3 +36,14 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+ document.querySelector('.deck').addEventListener('click', function(event){
+
+     if(event.target.classList.contains('card')){
+        let card = event.target;
+        card.classList.toggle('open');
+     }
+
+ });

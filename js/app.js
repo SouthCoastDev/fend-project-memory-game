39872631d9d354ad69a505a224894907c359a1d7
Alex <alexanderchapman@outlook.com>
2018-04-17 22:35:24 +0100

    feat: redesigned the js code.
    Probably a number of commits but I have redesigned the layout of the code and how it was working.
    Removed most of the functions and am working to try and make it work first and will then refactor into functions.
    at the moment all the logic is inside the event listener function.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
+
+
+    var playerGuess = [];
+    var moveCounter = 0;
+
+
+//  function flipCard(){
+//     document.querySelector('.deck').addEventListener('click', function(event){
+//         if(event.target.classList.contains('card')){
+//            let card = event.target;
+//            card.classList.toggle('open');
+//         }
+//     });    
+//  }
+
+function addCardToGuess(card){
+    playerGuess.push(card);
+}
+
+
+function matchTrue(){
+//clear array
+//lock cards open
+//add to list of matched cards
+//check if anything is left to match?
+//show win modal.
+}
+
+
+
+function increaseAttmept(){
+    moveCounter ++;
+}
+
+
+
+
+
+ /*
+ * Create a list that holds all of your cards
+ */
 var cardList = [
     "fa-diamond"
     ,"fa-paper-plane-o"
@@ -9,8 +50,7 @@ var cardList = [
     ,"fa-bomb"
 ];
 
-var playerGuess = [];
-var moveCounter = 0;
+var guessList = [];
 
 /*
  * Display the cards on the page
@@ -34,58 +74,67 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- * 
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- */
 
- function openCard(){
-    document.querySelector('.deck').addEventListener('click', function(event){
-        if(event.target.classList.contains('card')){
-           let card = event.target;
-           card.classList.toggle('open');
+/* set up the event listener for a card. If a card is clicked: */
+  document.querySelector('.deck').addEventListener('click', function(event){         
+ /*  - display the card's symbol (put this functionality in another function that you call from this one) */
+    let card = event.target;   
+    let cardFront = event.target.firstElementChild;
+    if(card.classList.contains('card')){
+        card.classList.toggle('open');
+     }
+ 
+ /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) */
+
+
+    if(cardFront.firstElementChild.classList.contains('fa')){
+        guessList.push(cardFront.firstElementChild.classList[1]);
+    }
+    
+    console.log(guessList);
+    console.log(guessList[0]);
+    console.log(guessList[1]);
+ 
+    /*  - if the list already has another card, check to see if the two cards match */
+
+    if(guessList.length > 1){
+
+        if(guessList[0] === guessList[1]){
+            console.log("Match");
+        } else{
+            console.log("No match");
+            
+        }   
+        
+      
+        
+    }
+    
+    if(guessList.length == 2) {
+        while(guessList.length > 0) {
+            guessList.pop();
         }
-    });
+    }
+    
 
-/*
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- */
+    });  //end event listener
+ 
 
-function addCardToGuess(card){
-    playerGuess.push(card);
-}
+ /*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ /*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)*/
+        
+ //empty guessList
+       
+ 
+ 
+/*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)*/
+
 
 function matchCheck(){
-    cardList[0] === cardList[1] ? true : false;    
+    guessList[0] == guessList[1] ? true : false; 
 }
 
-function matchTrue(){
-//clear array
-//lock cards open
-//add to list of matched cards
-//check if anything is left to match?
-//show win modal.
-}
 
-function mathFalse(){
-    //clear array
-    //flase flash
-}
-
-function increaseAttmept(){
-    moveCounter ++;
-}
-
- /*
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-}
 
  
